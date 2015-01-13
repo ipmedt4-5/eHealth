e6a05eb8071bec127c812d4845ed5e2382c783e6
Joost_van_eeden@hotmail.com <Joost_van_eeden@hotmail.com>
2015-01-13 14:59:12 +0100

    Versie 0.5 Database werkt Layout is compleet

--- e-Health/src/com/example/e_health/MainActivity.java
+++ e-Health/src/com/example/e_health/MainActivity.java
@@ -1,34 +1,71 @@
 package com.example.e_health;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.content.Intent;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+		
+		//button ezorg
+		Button btn = (Button) findViewById(R.id.eZorg);
+		btn.setOnClickListener(new View.OnClickListener() 
+		{
+		    public void onClick(View v) 
+		    {
+		    	Intent i = new Intent(MainActivity.this, Ezorg.class);
+		    	startActivity(i);
+		    }
+		});	
+		
+		//button epublichealth
+		Button eph = (Button) findViewById(R.id.ePublicHealth);
+		eph.setOnClickListener(new View.OnClickListener() 
+		{
+			public void onClick(View v) 
+			{
+				Intent i = new Intent(MainActivity.this,EpublicHealth.class);
+				startActivity(i);
+				
+			}
+		});
+		/*
+		//button zorgondersteuning
+				Button ezo = (Button) findViewById(R.id.eZorgOndersteuning);
+				eph.setOnClickListener(new View.OnClickListener() 
+				{
+					public void onClick(View v) 
+					{
+						Intent i = new Intent(MainActivity.this,Ezonderondersteuning.class);
+						startActivity(i);
+						
+					}
+				});
+				*/
+				
+		//button info
+		ImageButton info = (ImageButton)findViewById(R.id.info);
+		info.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(MainActivity.this,Help.class);
+				startActivity(i);
+				
+			}
+		});
+		
 	}
 }
