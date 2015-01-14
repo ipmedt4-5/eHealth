efa8bccf7ee44e67ceeb116fe373d66a0b76ac9a
Joost_van_eeden@hotmail.com <Joost_van_eeden@hotmail.com>
2015-01-14 16:23:56 +0100

    versie 0.9 EZorg is af

--- e-Health/src/com/example/e_health/Ecare.java
+++ e-Health/src/com/example/e_health/Ecare.java
@@ -1,7 +1,10 @@
 package com.example.e_health;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Ecare extends Activity
 {
@@ -9,5 +12,77 @@ public class Ecare extends Activity
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.ecare);
+		
+		//button ZWEcare
+		ImageButton zwec = (ImageButton) findViewById(R.id.zwecare);
+		zwec.setOnClickListener(new View.OnClickListener() 
+		{
+		   public void onClick(View v) 
+			  {
+			    Intent i = new Intent(Ecare.this, EcareZW.class);
+			    startActivity(i);
+			   }
+		});	
+
+	//button ZZEcare
+		ImageButton zzec = (ImageButton) findViewById(R.id.zzecare);
+		zzec.setOnClickListener(new View.OnClickListener() 
+		{
+			  public void onClick(View v) 
+			  {
+			    Intent i = new Intent(Ecare.this, EcareZZ.class);
+			    startActivity(i);
+			   }
+		});	
+	//button ZAEcare
+		ImageButton zaec = (ImageButton) findViewById(R.id.zaecare);
+		zaec.setOnClickListener(new View.OnClickListener() 
+		{
+		   public void onClick(View v) 
+			  {
+			    Intent i = new Intent(Ecare.this, EcareZA.class);
+			    startActivity(i);
+			   }
+		});	
+		//button ZPEcare
+		ImageButton zpec = (ImageButton) findViewById(R.id.zpecare);
+		zpec.setOnClickListener(new View.OnClickListener() 
+		{
+		   public void onClick(View v) 
+			  {
+			    Intent i = new Intent(Ecare.this, EcareZP.class);
+			    startActivity(i);
+			   }
+		});	
+		//button PLEcare
+		ImageButton plec = (ImageButton) findViewById(R.id.plecare);
+		plec.setOnClickListener(new View.OnClickListener() 
+		{
+		   public void onClick(View v) 
+			  {
+			    Intent i = new Intent(Ecare.this, EcarePL.class);
+			    startActivity(i);
+			   }
+		});	
+		//button PPEcare
+		ImageButton ppec = (ImageButton) findViewById(R.id.ppecare);
+		ppec.setOnClickListener(new View.OnClickListener() 
+		{
+		   public void onClick(View v) 
+			  {
+			    Intent i = new Intent(Ecare.this, EcarePP.class);
+			    startActivity(i);
+			   }
+		});	
+		//button PAEcare
+		ImageButton paec = (ImageButton) findViewById(R.id.paecare);
+		paec.setOnClickListener(new View.OnClickListener() 
+		{
+		   public void onClick(View v) 
+			  {
+			    Intent i = new Intent(Ecare.this, EcarePA.class);
+			    startActivity(i);
+			   }
+		});	
 	}
 }
--- e-Health/src/com/example/e_health/Etherapie.java
+++ e-Health/src/com/example/e_health/Etherapie.java
@@ -1,7 +1,10 @@
 package com.example.e_health;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Etherapie extends Activity
 {
@@ -9,6 +12,80 @@ public class Etherapie extends Activity
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.etherapie);
+		
+		//button zwetherapie
+		ImageButton zwet = (ImageButton) findViewById(R.id.zwetherapie);
+		zwet.setOnClickListener(new View.OnClickListener() 
+		{
+		   public void onClick(View v) 
+			  {
+			    Intent i = new Intent(Etherapie.this, EtherapieZW.class);
+			    startActivity(i);
+			   }
+		});	
+
+	//button ZZetherapie
+		ImageButton zzet = (ImageButton) findViewById(R.id.zzetherapie);
+		zzet.setOnClickListener(new View.OnClickListener() 
+		{
+			  public void onClick(View v) 
+			  {
+			    Intent i = new Intent(Etherapie.this, EtherapieZZ.class);
+			    startActivity(i);
+			   }
+		});	
+	//button ZAetherapie
+		ImageButton zaet = (ImageButton) findViewById(R.id.zaetherapie);
+		zaet.setOnClickListener(new View.OnClickListener() 
+		{
+		   public void onClick(View v) 
+			  {
+			    Intent i = new Intent(Etherapie.this, EtherapieZA.class);
+			    startActivity(i);
+			   }
+		});	
+		//button ZPetherapie
+		ImageButton zpet = (ImageButton) findViewById(R.id.zpetherapie);
+		zpet.setOnClickListener(new View.OnClickListener() 
+		{
+		   public void onClick(View v) 
+			  {
+			    Intent i = new Intent(Etherapie.this, EtherapieZP.class);
+			    startActivity(i);
+			   }
+		});	
+		//button PLetherapie
+		ImageButton plet = (ImageButton) findViewById(R.id.pletherapie);
+		plet.setOnClickListener(new View.OnClickListener() 
+		{
+		   public void onClick(View v) 
+			  {
+			    Intent i = new Intent(Etherapie.this, EtherapiePL.class);
+			    startActivity(i);
+			   }
+		});	
+		//button PPetherapie
+		ImageButton ppet = (ImageButton) findViewById(R.id.ppetherapie);
+		ppet.setOnClickListener(new View.OnClickListener() 
+		{
+		   public void onClick(View v) 
+			  {
+			    Intent i = new Intent(Etherapie.this, EtherapiePP.class);
+			    startActivity(i);
+			   }
+		});	
+		//button PAetherapie
+		ImageButton paet = (ImageButton) findViewById(R.id.paetherapie);
+		paet.setOnClickListener(new View.OnClickListener() 
+		{
+		   public void onClick(View v) 
+			  {
+			    Intent i = new Intent(Etherapie.this, EtherapiePA.class);
+			    startActivity(i);
+			   }
+		});	
 	}
 }
+
+
 
